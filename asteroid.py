512522785f3a240b3da0ebafc1bee9aa6587701f
eprellis98 <eprellis98@gmail.com>
2024-11-23 04:33:03 +0000

    F: split logic and kill logic

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        angle = random.uniform(20, 50)
+        v1 = pygame.Vector2(self.velocity).rotate(angle)
+        v2 = pygame.Vector2(self.velocity).rotate(angle * -1) 
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        a1 = Asteroid(self.position[0], self.position[1], new_radius)
+        a2 = Asteroid(self.position[0], self.position[1], new_radius)
+        v1 = v1 * 1.2
+        v2 = v2 * 1.2
+        a1.velocity = v1
+        a2.velocity = v2
+
+
+
+
